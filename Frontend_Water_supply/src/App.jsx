c51f32d28ffe8a1835e59ba8a0e3c2dd23191a9d
Roshnipatil31 <Roshni@digi9.co.in>
2025-03-15 15:38:00 +0530

    login page

--- Frontend_Water_supply/src/App.jsx
+++ Frontend_Water_supply/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/loginPage/loginPage';
+import Sample from './pages/sample/sample';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/sample" element={<Sample />} />
       </Routes>
     </Router>
   );
--- Frontend_Water_supply/src/pages/loginPage/loginPage.jsx
+++ Frontend_Water_supply/src/pages/loginPage/loginPage.jsx
@@ -1,45 +1,29 @@
-import React, { useState } from "react";
-import { Container, Form, Input, Button, Title } from "./loginPage.styles";
+import react from "react";  
+import { LoginBox, LoginDrop, Login, LoginContent, LoginTitle, InputBox, Input, Button } from "./smaple.styles";
 
-const LoginPage = () => {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-  });
-
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Login Data:", formData);
-  };
-
+const sample = () => {
   return (
-    <Container>
-      <Form onSubmit={handleSubmit}>
-        <Title>Login</Title>
-        <Input
-          type="email"
-          name="email"
-          placeholder="Email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-        <Input
-          type="password"
-          name="password"
-          placeholder="Password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-        <Button type="submit">Login</Button>
-      </Form>
-    </Container>
+    <Login>
+    <LoginBox>
+        <LoginDrop>
+            <LoginContent>
+                <LoginTitle>Sign In</LoginTitle>
+                <form action="">
+                    <InputBox>
+                        <Input type="text" placeholder="Username" />
+                    </InputBox>
+                    <InputBox>
+                        <Input type="password" placeholder="password" />
+                    </InputBox>
+                    <InputBox>
+                        <Button> Log In </Button>
+                    </InputBox>
+                </form>
+            </LoginContent>
+        </LoginDrop>
+    </LoginBox>
+    </Login>
   );
-};
+}
 
-export default LoginPage;
+export default sample;
